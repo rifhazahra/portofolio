0e40c51b0da5cd37c996d028b38a47040e13ac18
rifhazahra <rifhazahra6826@gmail.com>
2025-02-07 08:56:30 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Hamburger menu di klik
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+hamburgerMenu.onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar sidebar, menghilangkan nav
+document.addEventListener("click", function (e) {
+  if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+
+
+function klik(){
+    alert("Pesan Telah Dikirim")
+}
